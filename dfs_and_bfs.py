96f9b46b6c7e41fd30e10d5ce69f65f19b13ed77
hallgrimur1471 <hallgrimur1471@gmail.com>
2018-08-20 21:34:14 +0000

    Datastructures and algorithms

--- dfs_and_bfs.py
+++ dfs_and_bfs.py
@@ -1,1 +1,234 @@
 #!/usr/bin/env python3.5
+
+from collections import deque
+from copy import deepcopy
+
+"""
+Adjacency List
+No cycle detection
+"""
+
+def dfs1(G, s, t):
+    print(s)
+    if s == t:
+        return True
+    for u in G[s]:
+        if dfs1(G, u, t):
+            return True
+    return False
+
+def bfs1(G, s, t):
+    q = deque()
+    q.append(s)
+    while q:
+        k = q.popleft()
+        print(k)
+        if k == t:
+            return True
+        for u in G[k]:
+            q.append(u)
+    return False
+
+"""
+Adjacency List
+cycle detection
+"""
+
+def dfs2(G, s, t, v):
+    print(s)
+    v.add(s)
+    if s == t:
+        return True
+    for u in G[s]:
+        if u in v:
+            continue
+        if dfs2(G, u, t, v):
+            return True
+    return False
+
+def bfs2(G, s, t):
+    v = set()
+    q = deque()
+    q.append(s)
+    while q:
+        k = q.popleft()
+        v.add(k)
+        print(k)
+        if k == t:
+            return True
+        for u in G[k]:
+            if u in v:
+                continue
+            q.append(u)
+    return False
+
+"""
+Adjacency List with weights
+cycle detection
+"""
+
+def dfs3(G, s, t, v):
+    print(s)
+    v.add(s)
+    if s == t:
+        return True
+    for (u, d) in G[s]:
+        if u in v:
+            continue
+        if dfs3(G, u, t, v):
+            return True
+    return False
+
+def bfs3(G, s, t):
+    v = set()
+    q = deque()
+    q.appendleft(s)
+    while q:
+        k = q.pop()
+        v.add(k)
+        print(k)
+        if k == t:
+            return True
+        for (u, d) in G[k]:
+            if u in v:
+                continue
+            q.appendleft(u)
+    return False
+
+"""
+Adjacency Matrix with weights
+cycle detection
+"""
+
+def dfs4(G, s, t, v):
+    v.add(s)
+    print(s)
+    if s == t:
+        return True
+    for (i, u) in enumerate(G[s]):
+        if u <= 0:
+            continue
+        if i in v:
+            continue
+        if dfs4(G, i, t, v):
+            return True
+    return False
+
+def bfs4(G, s, t):
+    v = set()
+    q = deque()
+    q.appendleft(s)
+    while q:
+        k = q.pop()
+        v.add(k)
+        print(k)
+        if k == t:
+            return True
+        for (i, u) in enumerate(G[k]):
+            if u <= 0:
+                continue
+            if i in v:
+                continue
+            q.appendleft(i)
+    return False
+
+graph1 = dict()
+graph1[1] = [2,3,4]
+graph1[2] = [5,6]
+graph1[3] = [7,8]
+graph1[4] = [9,10]
+graph1[5] = []
+graph1[6] = []
+graph1[7] = []
+graph1[8] = []
+graph1[9] = []
+graph1[10] = []
+
+graph1_with_cycle = deepcopy(graph1)
+graph1_with_cycle[5] = [6]
+graph1_with_cycle[6] = [5]
+
+print("dfs1")
+dfs1(graph1, 1, 8)
+
+# would throw RecursionError:
+#dfs1(graph1_with_cycle, 1, 8)
+
+print("bfs1")
+bfs1(graph1, 1, 8)
+
+print("bfs1")
+# Might be checking the same nodes many times for some graphs
+bfs1(graph1_with_cycle, 1, 8)
+
+print("dfs2")
+dfs2(graph1, 1, 8, set())
+
+print("dfs2")
+dfs2(graph1_with_cycle, 1, 8, set())
+
+print("bfs2")
+bfs2(graph1, 1, 8)
+
+print("bfs2")
+bfs2(graph1_with_cycle, 1, 8)
+
+graph2 = dict()
+graph2[1] = [(2, 3), (3, 3), (4, 3)]
+graph2[2] = [(5, 2), (6, 3)]
+graph2[3] = [(7, 5), (8, 2)]
+graph2[4] = [(9, 15), (10, 20)]
+graph2[5] = []
+graph2[6] = []
+graph2[7] = []
+graph2[8] = []
+graph2[9] = []
+graph2[10] = []
+
+graph2_with_cycle = deepcopy(graph2)
+graph2_with_cycle[5] = [(6, 100)]
+graph2_with_cycle[6] = [(5, 100)]
+
+print("dfs3")
+dfs3(graph2, 1, 8, set())
+print("dfs3")
+dfs3(graph2_with_cycle, 1, 8, set())
+print("bfs3")
+bfs3(graph2, 1, 8)
+print("bfs3")
+bfs3(graph2_with_cycle, 1, 8)
+
+graph3 = [
+    [0, 3, 3, 3, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 2, 3, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 5, 2, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 15, 20],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+]
+
+graph3_with_cycle = [
+    [0, 3, 3, 3, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 2, 3, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 5, 2, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 15, 20],
+    [0, 0, 0, 0, 0, 100, 0, 0, 0, 0],
+    [0, 0, 0, 0, 100, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+]
+
+print("dfs4")
+dfs4(graph3, 0, 7, set())
+print("dfs4")
+dfs4(graph3_with_cycle, 0, 7, set())
+print("bfs4")
+bfs4(graph3, 0, 7)
+print("bfs4")
+bfs4(graph3_with_cycle, 0, 7)
